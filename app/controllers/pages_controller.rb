1e0417fd44cf239628c8dc0d55ddfddcaae50e37
Maria Bohorquez <a.maria.bohorquez@gmail.com>
2021-07-01 20:32:11 +0200

    coach

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def ask
+    # fetch user's input
+    params[:userstatement]
+  end
+
+  def answer
+    # display user's input
+    # display coach's answer
+  end
+end
